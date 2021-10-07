ed4b57abe699173d945f80816c1386d823aa8bd3
Max Wolter <max@flare.network>
2021-10-07 13:33:13 +0200

    Update to Coreth version with fixed websocket timeout

--- cmd/songbird.sh
+++ cmd/songbird.sh
@@ -4,13 +4,12 @@
 # Please see the file LICENSE for licensing terms.
 
 if [[ $(pwd) =~ " " ]]; then echo "Working directory path contains a folder with a space in its name, please remove all spaces" && exit; fi
-if [ -z ${GOPATH+x} ]; then echo "GOPATH is not set, visit https://github.com/golang/go/wiki/SettingGOPATH" && exit; fi
 printf "\x1b[34mSongbird Canary Network Deployment\x1b[0m\n\n"
 
 LAUNCH_DIR=$(pwd)
 
 DB_TYPE=rocksdb
-if [ "$(uname)" == "Darwin" ]; then DB_TYPE=leveldb; fi
+if [ "$(uname)" != "Linux" ] || [ "$(arch)" != "x86_64"]; then DB_TYPE=leveldb; fi
 
 # Test and export underlying chain APIs you chose to use for the state connector
 source ./conf/export_chain_apis.sh $LAUNCH_DIR/conf/songbird/chain_apis.json
@@ -30,14 +29,14 @@ fi
 
 # NODE 1
 printf "Launching Songbird Node at 127.0.0.1:9650\n"
-export WEB3_API=debug
+export WEB3_API=enabled
 nohup ./build/avalanchego \
 --http-host= \
 --public-ip=127.0.0.1 \
 --http-port=9650 \
 --staking-port=9651 \
---log-dir=$LAUNCH_DIR/logs/songbird/node1 \
---db-dir=$LAUNCH_DIR/db/songbird/node1 \
+--log-dir=$LAUNCH_DIR/logs/songbird \
+--db-dir=$LAUNCH_DIR/db/songbird \
 --bootstrap-ips="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeIP" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.ip")" \
 --bootstrap-ids="$(curl -m 10 -sX POST --data '{ "jsonrpc":"2.0", "id":1, "method":"info.getNodeID" }' -H 'content-type:application/json;' https://songbird.flare.network/ext/info | jq -r ".result.nodeID")" \
 --db-type=$DB_TYPE \
